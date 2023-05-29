85c34812b000b6fd121b23f3eb22d012a1913bd3
CalabrianChopper <francesco.chiodo.op@gmail.com>
2023-05-29 10:30:27 +0200

    inizializzazione

---
+++ script.js
@@ -0,0 +1,9 @@
+// function for sidebar
+
+const sidebar = document.querySelector('.sidebar');
+  const toggleBtn = document.querySelector('.toggle-btn');
+
+  toggleBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+});
+
